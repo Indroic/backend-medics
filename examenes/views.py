ba2bb773f86fd5c1020472cb6fce4832ce416ac1
Indroic <davidlatosefkindriago@gmail.com>
2024-10-20 20:45:14 -0400

    2 pajaros de un tiro

--- examenes/views.py
+++ examenes/views.py
@@ -5,16 +5,33 @@ from rest_framework.response import Response
 from .serializers import ExamenSerializer, CategoriaSerializer, ExamenListSerializer
 from .models import Examen, Categoria
 
+
 class ExamenViewSet(ModelViewSet):
     queryset = Examen.objects.all()
     serializer_class = ExamenSerializer
     permission_classes = [IsAuthenticated]
-    
+
     def list(self, request, *args, **kwargs):
         examenes = Examen.objects.all().filter(agregado_por=self.request.user)
-        
+
         return Response({"examenes": ExamenListSerializer(examenes, many=True).data})
-    
+
+    def create(self, request, *args, **kwargs):
+        request.data["agregado_por"] = self.request.user.id
+        medico = Examen.objects.get(
+            id=super().create(request, *args, **kwargs).data["id"]
+        )
+
+        return Response(data=ExamenListSerializer(medico).data)
+
+    def partial_update(self, request, *args, **kwargs):
+        medico = Examen.objects.get(
+            id=super().partial_update(request, *args, **kwargs).data["id"]
+        )
+
+        return Response(data=ExamenListSerializer(medico).data)
+
+
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
--- medicos/views.py
+++ medicos/views.py
@@ -11,22 +11,27 @@ class MedicoViewSet(ModelViewSet):
     queryset = Medico.objects.all()
     serializer_class = MedicoSerializer
     permission_classes = [IsAuthenticated]
-    
-    
+
     def list(self, request, *args, **kwargs):
         medicos = Medico.objects.all().filter(agregado_por=self.request.user)
-        
-        return Response({"medicos": MedicoListSerializer(medicos, many=True).data})
-    
-    def create(self, request: Request, *args, **kwargs):
-        request.data["agregado_por"] = self.request.user.id
 
-        return super().create(request, *args, **kwargs)
-    def partial_update(self, request: Request, *args, **kwargs):
-        medico = Medico.objects.get(id=super().partial_update(request, *args, **kwargs).data["id"])
-        
+        return Response({"medicos": MedicoListSerializer(medicos, many=True).data})
+
+    def create(self, request, *args, **kwargs):
+        request.data["agregado_por"] = self.request.user.id
+        medico = Medico.objects.get(
+            id=super().create(request, *args, **kwargs).data["id"]
+        )
 
         return Response(data=MedicoListSerializer(medico).data)
+
+    def partial_update(self, request, *args, **kwargs):
+        medico = Medico.objects.get(
+            id=super().partial_update(request, *args, **kwargs).data["id"]
+        )
+
+        return Response(data=MedicoListSerializer(medico).data)
+
 
 class EspecialidadViewSet(ModelViewSet):
     queryset = Especialidad.objects.all()
