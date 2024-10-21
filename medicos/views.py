3c4b8286c4c7ac288d03eed6f128ce8d5bda7e99
Indroic <davidlatosefkindriago@gmail.com>
2024-10-20 20:41:17 -0400

    ahroa si, corregido

--- medicos/views.py
+++ medicos/views.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.request import Request
 
 from .serializers import MedicoSerializer, EspecialidadSerializer, MedicoListSerializer
 from .models import Medico, Especialidad
@@ -17,16 +18,15 @@ class MedicoViewSet(ModelViewSet):
         
         return Response({"medicos": MedicoListSerializer(medicos, many=True).data})
     
-    def create(self, request, *args, **kwargs):
+    def create(self, request: Request, *args, **kwargs):
         request.data["agregado_por"] = self.request.user.id
 
         return super().create(request, *args, **kwargs)
-    def partial_update(self, request, *args, **kwargs):
-        super().partial_update(request, *args, **kwargs)
-        medicoActualizado = Medico.objects.get(id=request.data["id"])
+    def partial_update(self, request: Request, *args, **kwargs):
+        medico = Medico.objects.get(id=super().partial_update(request, *args, **kwargs).data["id"])
         
 
-        return Response({"medico": MedicoListSerializer(medicoActualizado).data})
+        return Response(data=MedicoListSerializer(medico).data)
 
 class EspecialidadViewSet(ModelViewSet):
     queryset = Especialidad.objects.all()
