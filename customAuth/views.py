17ca36871877721bc0bb9485319931962b76c7ce
Indroic <davidlatosefkindriago@gmail.com>
2024-11-04 20:22:45 -0400

    asdsad

--- customAuth/views.py
+++ customAuth/views.py
@@ -49,7 +49,6 @@ def register(request, *args, **kwargs):
             ci=serializer.validated_data["ci"],
             genero=serializer.validated_data["genero"],
             telefono=serializer.validated_data["telefono"],
-            alergias=serializer.validated_data["alergias"],
             patologia=serializer.validated_data["patologia"],
             avatar=serializer.validated_data["avatar"],
         )
